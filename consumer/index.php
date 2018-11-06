ce63fc41e759802ea6032965306992a538c1039b
Julien <julien.acroute@camptocamp.com>
2018-11-06 11:19:00 +0100

    Add env var to configure port

--- consumer/index.php
+++ consumer/index.php
@@ -11,9 +11,12 @@
             $producer = getenv('PRODUCER_SERVICE');
             if(!$producer)
               $producer = 'producer';
+            $producer_port = getenv('PRODUCER_PORT');
+            if(!$producer_port)
+              $producer_port = '8080';
 
             // Request products from producer
-            $response = json_decode(file_get_contents("http://$producer:8080/"));
+            $response = json_decode(file_get_contents("http://$producer:$producer_port/"));
             // Display results
             foreach ($response->products as $product)
                 echo "<li>$product</li>\n";
