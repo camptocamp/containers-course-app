50afa0d42842d7154ddde6b0dfefbbb4b865ace0
Julien Acroute <julien.acroute@camptocamp.com>
2019-11-23 21:42:26 +0100

    Add content-type

--- producer/api.py
+++ producer/api.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def data():
-  with open("/etc/producer/data.json","r") as file:
-    return file.read()
+    with open("/etc/producer/data.json", "r") as file:
+        headers = {'Content-Type': 'application/json'}
+        return file.read(), 200, headers
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
