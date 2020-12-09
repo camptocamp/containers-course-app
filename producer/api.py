18be8405dbbe40f8b3462d57cfa23b0978dd4f53
Julien Acroute <julien.acroute@camptocamp.com>
2020-12-09 10:11:12 +0100

    Add buy, description and stats on producer

--- producer/api.py
+++ producer/api.py
@@ -1,11 +1,58 @@
 from flask import Flask
+import json
+
 
 app = Flask(__name__)
+data_path = "/etc/producer/data.json"
+data_path = "/home/jacroute/git/course/docker/producer/data.json"
+
+products_view = {}
+products_buy = {}
+
+@app.route('/products/<product>')
+def view_product(product):
+    try:
+        p = load_product(product)
+        # Update stats
+        if product not in products_view:
+            products_view[product] = 1
+        else:
+            products_view[product] = products_view[product] + 1
+        # Add stats
+        p['view'] = products_view[product] if product in products_view else 0
+        p['buy'] = products_buy[product] if product in products_buy else 0
+        headers = {'Content-Type': 'application/json'}
+        return json.dumps(p), 200, headers
+    except ValueError:
+        return "No such product", 404
+
+
+@app.route('/buy/<product>')
+def buy_product(product):
+    try:
+        p = load_product(product)
+        # Update stats
+        if product not in products_buy:
+            products_buy[product] = 1
+        else:
+            products_buy[product] = products_buy[product] + 1
+        return 'Thanks for buying opensource software ;-)'
+    except ValueError:
+        return "No such product", 404
+
+
+def load_product(product):
+    with open(data_path, "r") as file:
+        json_products = json.loads(file.read())
+        for p in json_products['products']:
+            if p['name'] == product:
+                return p
+        raise ValueError('Product %s not found' % product)
 
 
 @app.route("/")
-def data():
-    with open("/etc/producer/data.json", "r") as file:
+def products():
+    with open(data_path, "r") as file:
         headers = {'Content-Type': 'application/json'}
         return file.read(), 200, headers
 
