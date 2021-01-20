eece61e6c9c0239451a7f30e8f6e85eb939d5133
Raphaël Pinson <raphael.pinson@camptocamp.com>
2021-01-20 17:32:46 +0100

    Use navbar-brand as home link

--- frontend/navbar.php
+++ frontend/navbar.php
@@ -1,12 +1,7 @@
 <div class="navbar navbar-default">
   <div class="container">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">C2C projects</a>
+      <a class="navbar-brand" href="/">C2C projects</a>
     </div>
-    <ul class="nav navbar-nav navbar-collapse collapse">
-      <li>
-        <a href="/">Products list</a>
-      </li>
-    </ul>
   </div>
 </div>
