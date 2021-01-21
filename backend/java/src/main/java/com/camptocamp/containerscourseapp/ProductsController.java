c9d87a84b83ad1ce5989b190ed922d5bca3f6c00
Aurelien Campergue <aurelien.campergue@camptocamp.com>
2021-01-21 17:45:12 +0100

    Implement the /buy/<product> and simplify the whole code

--- backend/java/src/main/java/com/camptocamp/containerscourseapp/ProductsController.java
+++ backend/java/src/main/java/com/camptocamp/containerscourseapp/ProductsController.java
@@ -3,15 +3,12 @@ package com.camptocamp.containerscourseapp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,24 +18,12 @@ import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-@RestController
-public class ProductsController {
-
-    
-    final Logger LOGGER = LoggerFactory.getLogger(getClass());
-
-    private static final Map<String,Integer> views = new HashMap<String,Integer>(); //number of views
-    private static final Map<String,Integer> buys = new HashMap<String,Integer>(); //number of buys
-    // private static final Map<String, Product> allProducts = new HashMap<String,Product>(); //all Product objects, indexed by their name
 
 //  * App requirements:
 
@@ -48,46 +33,30 @@ public class ProductsController {
    
 //  * '/buy/<product>': buy a product, call to this page increment buy stats
 //      * return 200 with html
+@RestController
+public class ProductsController {
 
-    public ProductsController() {
-        LOGGER.info("init map");
-        // init from json
-        views.put("geomapfish",0);
-        views.put("georchestra",0);
-        views.put("geonetwork",0);
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
-        buys.put("geomapfish",0);
-        buys.put("georchestra",0);
-        buys.put("geonetwork",0);
-    }
+    private static final String dataFileName = "data.json";
+    private static final Map<String,Integer> views = new HashMap<String,Integer>(); //number of views
+    private static final Map<String,Integer> buys = new HashMap<String,Integer>(); //number of buys
 
     @Autowired
     ResourceLoader resourceLoader;
 
+
     @RequestMapping("/")
-	public String index() {
-        JSONParser jsonParser = new JSONParser();
-        Resource resource = resourceLoader.getResource("classpath:data.json");
-        String data = null;
-        try {
-            InputStream inputStream = resource.getInputStream();
-            byte[] bdata = FileCopyUtils.copyToByteArray(inputStream);
-            data = new String(bdata, StandardCharsets.UTF_8);
-            LOGGER.info(data);
-        } catch (IOException e) {
-            LOGGER.error("IOException", e);
-        }
-        return data;
+	public String index() throws IOException, ParseException {
+        return this.readProductList().toJSONString();
     }
 
     @GetMapping("/products/{product}")
     // * '/product/<product>': describe one product and display stats (view and buy). call to this page increment view stats
     // * return json with addtional fileds: view and buy
-    public ResponseEntity<String> read(@PathVariable("product") String product) {
-
-        // Debug display of views/buys content 
-        LOGGER.info(Arrays.toString((views.keySet().toArray(new String[0]))));
-        LOGGER.info(Arrays.toString(views.values().toArray(new Integer[0])));
+    public ResponseEntity<String> getProduct(@PathVariable("product") String product)
+            throws IOException, ParseException {
+        readProductList(); //read or re-read product list to make sure stats are initialized
      
         if (!views.containsKey(product)) {
             LOGGER.error(product + " not found !");
@@ -95,12 +64,88 @@ public class ProductsController {
         }
         int productViews = views.get(product);
         views.put(product, productViews+1);
+    
+        return ResponseEntity.ok(formatResponse(product));
+    }
 
+
+    @GetMapping("/buy/{product}")
+    //  * '/buy/<product>': buy a product, call to this page increment buy stats
+    //  * return 200 with html
+    public ResponseEntity<String> buyProduct(@PathVariable("product") String product)
+            throws IOException, ParseException {
+        readProductList(); //read or re-read product list to make sure stats are initialized
+     
+        if (!buys.containsKey(product)) {
+            LOGGER.error(product + " not found !");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        int productBought = buys.get(product);
+        buys.put(product, productBought+1);
+    
+        return ResponseEntity.ok(formatResponse(product));
+    }
+
+    private String formatResponse(String product) {
         StringBuilder sb = new StringBuilder();
         sb.append(product)
             .append(" viewed : ").append(Integer.toString(views.get(product)))
             .append(" bought : ").append(Integer.toString(buys.get(product)));
-    
-        return ResponseEntity.ok(sb.toString());
+        return sb.toString();
     }
+
+    private JSONObject readProductList() throws IOException, ParseException {
+        Resource resource = resourceLoader.getResource("classpath:" + dataFileName);
+        return readDataJson(resource);
+    }
+
+    private JSONObject readDataJson(Resource resource) throws IOException, ParseException {
+        JSONObject productList = null;
+        JSONParser jsonParser = new JSONParser();
+        try (Reader reader = new InputStreamReader(resource.getInputStream()))
+        {
+            Object obj = jsonParser.parse(reader);
+ 
+            productList = (JSONObject) obj;
+            parseProductList(productList); 
+        } catch (FileNotFoundException e) {
+            LOGGER.error("FileNotFoundException",e);
+            throw e;
+        } catch (IOException e) {
+            LOGGER.error("IOException",e);
+            throw e;
+        } catch (ParseException e) {
+            LOGGER.error("ParseException",e);
+            throw e;
+        }
+        return productList;
+    }
+
+    private void parseProductList(JSONObject productList) 
+    {
+        //Get product object within list
+        JSONArray productArray = (JSONArray) productList.get("products");
+
+        //Iterate over employee array
+        productArray.forEach( productObject -> parseProduct( (JSONObject) productObject ) );
+    }
+
+    private void parseProduct(JSONObject productObject) {
+        String name = (String) productObject.get("name");    
+        // String desc = (String) productObject.get("description");
+        // Product product = new Product(name,desc);
+        initStats(name);
+    }
+    
+    private void initStats(String name) {
+        if (!views.containsKey(name)) {
+            LOGGER.info("Initialize viewing stats for " + name);
+            views.put(name, 0);
+        }
+        if (!buys.containsKey(name)) {
+            LOGGER.info("Initialize buying stats for " + name);
+            buys.put(name, 0);
+        }
+    }
+
 }
